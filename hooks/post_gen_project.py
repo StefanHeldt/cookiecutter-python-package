8e1948b9e78f6e470fb203a3a8cebd773c332911
Stefan.Heldt <24435695+StefanHeldt@users.noreply.github.com>
2022-06-19 17:07:43 +0100

    Add git repo generation

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""Script is run after cookiecutter creates repository."""
+import os
+
+
+repo_name = '{{ cookiecutter.project_name }}'
+setup_git = True if '{{ cookiecutter.setup_git_repo }}' == "Yes" else False
+
+if setup_git:
+    err = os.system('bash ./setup_git_repo.sh %s' %repo_name)
+    if err:
+        print("Error with GitHub repository setup!")
+    os.remove("./setup_git_repo.sh")
+
+os.system("cd %s" %repo_name)
