f72c052794ecb8715efbc09c66429ce94765907b
Stefan.Heldt <24435695+StefanHeldt@users.noreply.github.com>
2022-07-23 10:39:16 +0100

    Add parsing of command line arguments

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__main__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__main__.py
@@ -1,12 +1,58 @@
+import argparse
 import logging.config
-from {{cookiecutter.package_name}}.logging import LOGGING_CONFIG
+import sys
 
+from {{cookiecutter.package_name}}.logging import LOGGING_CONFIG
+from {{cookiecutter.package_name}} import __version__
+from typing import List
 
 logger = logging.getLogger("{{cookiecutter.package_name}}")
 
 
+def parse_args(args: List[str]) -> argparse.Namespace:
+    """Parse command line arguments and parameters.
+
+    Args:
+      args ([str]): command line parameters as list of strings.
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace.
+
+    """
+    parser = argparse.ArgumentParser(
+        description="{{cookiecutter.project_short_description}}"
+    )
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="%(prog)s (" + __version__ + ")",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="loglevel",
+        help="set loglevel to INFO",
+        default=logging.root.level,
+        action="store_const",
+        const=logging.INFO,
+    )
+    parser.add_argument(
+        "-vv",
+        "--very-verbose",
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        default=logging.root.level,
+        action="store_const",
+        const=logging.DEBUG,
+    )
+
+    return parser.parse_args(args)
+
+
 def main() -> None:
     logging.config.dictConfig(LOGGING_CONFIG)
+    args = parse_args(sys.argv[1:])
+    logger.setLevel(args.loglevel)
+
     logger.info("Starting...")
 
 
