27dcede421fb63b3e36998df280f290bf7baa579
Stefan Heldt <24435695+StefanHeldt@users.noreply.github.com>
2023-08-06 12:10:13 +0100

    Fix/small update (#3)
    * Ignore .env files
    
    * Ignore .env files
    
    * Set to latest poetry version
    
    * Don't fix poetry version in docker
    
    * Remove wrong cd command
    
    * Ignore setup_git_repo
    
    * Use GitHub CLIN for package setupt
    
    * Fix typo
    
    * Update packages
    
    * Run black
    
    * Add toml

--- {{cookiecutter.project_name}}/setup_git_repo.sh
+++ {{cookiecutter.project_name}}/setup_git_repo.sh
@@ -15,15 +15,9 @@ if [ "$repo_name" = "" ]; then
 repo_name=$dir_name
 fi
 
-username=`git config github.user`
-if [ "$username" = "" ]; then
+user_name=`git config github.user`
+if [ "$user_name" = "" ]; then
     echo "Could not find username, run 'git config --global github.user <username>'"
-    invalid_credentials=1
-fi
-
-token=`git config github.token`
-if [ "$token" = "" ]; then
-    echo "Could not find token, run 'git config --global github.token <token>'"
     invalid_credentials=1
 fi
 
@@ -31,14 +25,30 @@ if [ "$invalid_credentials" == "1" ]; then
     exit 1
 fi
 
-echo -n "Creating Github repository '$repo_name' ..."
-curl -u "$username:$token" https://api.github.com/user/repos -d '{"name":"'$repo_name'", "private":true}' > /dev/null 2>&1
-echo " done."
+if ! command -v gh &> /dev/null 2>&1
+then
+    echo "Could not find GitHub CLI, attempting to install..."
+    brew install gh
+fi
 
-echo "Pushing local code to remote ..."
+if ! gh auth status >/dev/null 2>&1
+then
+    echo "You need to login to GitHub."
+    gh auth login
+fi
+
+echo -n "Creating local Github repository '$repo_name' ..."
 git init .
 git add .
 git commit -m "initial setup"
-git remote add origin git@github.com:$username/$repo_name.git > /dev/null 2>&1
+git branch -M master
+echo "Done!"
+
+echo -n "Creating remote Github repository '$repo_name' ..."
+gh repo create $repo_name --private --source=. --remote=upstream
+echo " done."
+
+echo -n "Pushing code to remote ..."
+git remote add origin https://github.com/$user_name/$repo_name.git > /dev/null 2>&1
 git push -u origin master > /dev/null 2>&1
 echo "Done!"
